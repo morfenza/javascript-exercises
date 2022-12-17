6a4bb21a512eab568435a06035094a1c85f3f309
Morfenza <ianlourenco@protonmail.com>
2022-12-17 12:00:25 -0300

    Do exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+  
+  if (string === '') return '';
 
+  if (num < 0) return 'ERROR';
+
+  answer = '';
+
+  for (let i = 0; i < num; i++) {
+    answer += string;
+  }
+
+  return answer;
 };
 
 // Do not edit below this line
