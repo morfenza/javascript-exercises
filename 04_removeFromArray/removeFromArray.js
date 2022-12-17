8489aa28d37aa1508976f13c109db39484de46fb
Morfenza <ianlourenco@protonmail.com>
2022-12-17 13:06:46 -0300

    Do exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...values) {
+
+  let answer = array.filter((element) => {
+    
+    return !values.includes(element)
+
+  });
+
+  return answer
 
 };
+
+
+// Funciona em uma linha utilizando:
+// const removeFromArray = (array, ...values) => (array.filter((element) => !values.includes(element)));
 
 // Do not edit below this line
 module.exports = removeFromArray;
