6ed7b70b33208179d2292627a826349d9ec1802d
Morfenza <ianlourenco@protonmail.com>
2022-12-17 12:25:10 -0300

    Do exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,17 @@
-const reverseString = function() {
+const reverseString = function(string) {
+  
+  if (string === '') return '';
+  
+  const stringArray = string.split('');
+  const answerArray = [];
+
+  for (let i = stringArray.length - 1; i >= 0; i--) {
+    answerArray.push(stringArray[i]);
+  }
+
+  let answerString = answerArray.join('');
+
+  return answerString;
 
 };
 
