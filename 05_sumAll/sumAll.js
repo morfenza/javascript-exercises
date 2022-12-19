91f19ebd6e4a3539078e412867f6a360b8409ca7
Morfenza <ianlourenco@protonmail.com>
2022-12-19 12:57:29 -0300

    Do exercise 05 && add extra text

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(init, last) {
+  let sum = 0;
+  let counter = 0;
 
+  if (init < 0 || last < 0 || typeof init !== 'number' || typeof last !== 'number'){
+    return "ERROR";
+  }
+
+  if (init < last) {
+    counter = init;
+
+    while (counter <= last) {
+      sum += counter;
+      counter++;
+    }
+  } else if (init > last) {
+    counter = last;
+      
+    while (counter <= init) {
+      sum += counter;
+      counter++;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
