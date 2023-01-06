9494088a8392aa5d92ca1a85356fd3a6cf264a12
Morfenza <ianlourenco@protonmail.com>
2023-01-06 18:13:09 -0300

    Do exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let total = arr.reduce((acc, cur) => {
+    return acc += cur
+  }, 0);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let total = arr.reduce((acc, cur) => {
+    return acc *= cur
+  });
+  return total;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  let cur = 1;
+  while (cur <= a) {
+    total = total * cur
+    cur++
+  }
+  return total
 };
 
 // Do not edit below this line
