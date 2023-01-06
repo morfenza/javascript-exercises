307e7973af064b62d4a1bbbbc5d7aa7ffe64efd1
Morfenza <ianlourenco@protonmail.com>
2023-01-06 19:46:53 -0300

    Do exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+  
+  let titles = []
 
+  arr.forEach(element => {
+    titles.push(element.title)
+  });
+
+  return titles
 };
+
+// alternative -> return array.map(book => book.title);
 
 // Do not edit below this line
 module.exports = getTheTitles;
