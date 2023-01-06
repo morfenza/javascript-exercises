e771a1868dd1b271987a2c69ae39665070a8f24e
Morfenza <ianlourenco@protonmail.com>
2023-01-06 18:55:14 -0300

    Do exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+  aux = string.toLowerCase().replace(/[^a-z]/g, "")
+
+  if ((aux) === (aux.split('').reverse().join(''))) {
+    return true
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
