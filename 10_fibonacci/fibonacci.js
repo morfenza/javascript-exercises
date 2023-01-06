4f17a1fe1771172b301718be4b43529a9f16f505
Morfenza <ianlourenco@protonmail.com>
2023-01-06 19:32:00 -0300

    Do exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(max) {
+  if (max < 0) return 'OOPS';
+
+  const arr = [1, 1];
+  
+  while (arr.length < max) {
+    aux = arr[arr.length - 1] + arr[arr.length - 2]
+    arr.push(aux)
+  }
+  
+  return arr[arr.length - 1]
 
 };
 
